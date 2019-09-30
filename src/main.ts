432354d3a69c37cc2c818f1bc22b04a5673f5121
bokuweb <bokuweb12@gmail.com>
2019-09-30 19:16:51 +0900

    notif

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,8 @@ if (!event) {
   throw new Error("Failed to get github event.json..");
 }
 
+let succeeded = false;
+
 const run = async () => {
   const currentHash =
     event.after ||
@@ -29,16 +31,12 @@ const run = async () => {
       event.pull_request.head &&
       event.pull_request.head.sha);
 
-  console.log("current hash", currentHash);
   const [owner, reponame] = event.repository.full_name.split("/");
   const url = `https://github.com/${owner}/${reponame}/commit/${currentHash}`;
 
-  console.log("checks url", url);
   const { data } = await axios(url);
-  console.log("data", data);
   const $ = cheerio.load(data);
   $("a").each(async (i, elem) => {
-    console.log("++");
     if ($(elem).text() === "my-artifact") {
       const href = $(elem)[0].attribs.href;
       console.log("href", href);
@@ -49,8 +47,15 @@ const run = async () => {
           href
         )}`
       });
+      succeeded = true;
     }
   });
 };
 
-run();
+for (let i = 0; i < 3; i++) {
+  if (!succeeded) {
+    setTimeout(async () => {
+      await run();
+    }, i * 1000);
+  }
+}
